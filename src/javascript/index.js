adfbb7390d60a82368ffd4b324e1444d836ef4ab
Jaakal <jaak.kivinukk@gmail.com>
2020-04-08 21:31:43 +0300

    Added the group by projects functionality and changed the project logic

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -13,12 +13,14 @@ import $ from 'jquery';
 import todoCard from './todo-card';
 import projectCard from './project';
 
-const projects = [];
+let projects = [];
 let activities = [];
+
+let _projectID;
 
 let currentCard;
 
-const displayCards = () => {
+const displayCards = (projectID) => {
   $('.todo-table').html(`
     <tr>
       <th>Project</th>
@@ -30,8 +32,23 @@ const displayCards = () => {
     </tr>
   `);
 
-  for(let i = 0; i < activities.length; i += 1) {
-    const card = activities[i];
+  
+  
+  const filteredActivities = Number(projectID) !== -1 ? activities.filter((activity) => {
+    // console.log(activity);
+    if (activity) {
+      return activity.getProject().index === Number(projectID);
+    }
+  }) : activities;
+
+  // const filteredActivities = Number(projectID) !== projects.length ? activities.filter((activity) => {
+  //   if (activity) {
+  //     return activity.getProject().index === Number(projectID)
+  //   }
+  // }) : activities;
+  
+  for(let i = 0; i < filteredActivities.length; i += 1) {
+    const card = filteredActivities[i];
     if (card) {
       $('.todo-table').append(card.getHTML());
     }
@@ -41,7 +58,7 @@ const displayCards = () => {
   $('.delete-button').click(removeActivity);
 }
 
-const addProject = (event) => {
+const addProject = () => {
   $('.add-activity').addClass('hide');
   $('.add-project').removeClass('hide');
 }
@@ -51,10 +68,25 @@ const submitProject = (event) => {
 
   $('.add-project').addClass('hide');
 
-  const project = projectCard($('#add-project').val(), projects.length);
-  
+  const project = projectCard($('#add-project').val(), _projectID);
+
   projects.push(project);
   $('#project-list').append(`<option value=${project.index}>${project.name}</option>`);
+  $('#choose-project-list').append(`<option value=${project.index}>${project.name}</option>`);
+
+  localStorage.setItem("projects", JSON.stringify(projects));
+  
+  _projectID += 1;
+  localStorage.setItem("_projectsID", JSON.stringify(_projectID));
+}
+const updateLocalStorage = () => {
+  const literalArray = activities.map((card) => {
+    if (card) {
+      return card.getLiteral();
+    }
+    return null;
+  });
+  localStorage.setItem("activities", JSON.stringify(literalArray));
 }
 
 const addActivity = () => {
@@ -64,28 +96,29 @@ const addActivity = () => {
   $('.add-activity').removeClass('hide');
 }
 
-const updateLocalStorage = () => {
-  const literalArray = activities.map((card) => {
-    if(card) {
-      return card = card.getLiteral();
-    }
-    return null;
-  });
-  localStorage.setItem("activities", JSON.stringify(literalArray));
-}
 
 const submitActivity = (event) => {
   event.preventDefault();
   $('.add-activity').addClass('hide');
 
   const activityData = $('.add-activity').serializeArray();
+  
   let currentIndex = currentCard.getIndex();
+  let displayCardsCurrentProject = $('#choose-project-list').val();
+
   if (currentIndex === undefined) {
     currentIndex = activities.length;
     activities.push(currentCard);
+    displayCardsCurrentProject = activityData[0].value;
   }
 
-  const cardData = [projects[activityData[0].value], 
+  const filteredProject = projects.filter((project) => {
+      return Number(project.index) === Number(activityData[0].value);
+  })[0];
+
+  console.log(filteredProject);
+
+  const cardData = [filteredProject, 
                     activityData[1].value, 
                     activityData[2].value, 
                     activityData[3].value, 
@@ -93,10 +126,9 @@ const submitActivity = (event) => {
                     currentIndex
                    ];
 
-
   currentCard.setCard(cardData);
   updateLocalStorage();
-  displayCards();
+  displayCards(displayCardsCurrentProject);
 }
 
 const editActivity = (event) => {
@@ -115,9 +147,9 @@ const removeActivity = (event) => {
   const card = activities[$(event.target).data('index')];
   activities[card.getIndex()] = null;
   updateLocalStorage();
-  displayCards();
+  
+  displayCards($('#choose-project-list').val());
 }
-
 
 $(document).ready(() => {
   $('.add-project-button').click(addProject);
@@ -136,43 +168,46 @@ $(document).ready(() => {
   projects.push(project);
   $('#project-list').append(`<option value=${project.index}>${project.name}</option>`);
 
-  project = projectCard('Programming', 1);
-  projects.push(project);
-  $('#project-list').append(`<option value=${project.index}>${project.name}</option>`);
-  
-  project = projectCard('Cooking', 2);
-  projects.push(project);
-  $('#project-list').append(`<option value=${project.index}>${project.name}</option>`);
-  
-  project = projectCard('Dancing', 3);
-  projects.push(project);
-  $('#project-list').append(`<option value=${project.index}>${project.name}</option>`);
 
-  let todo = todoCard();
+  _projectID = Number(localStorage.getItem("_projectsID"));
+  _projectID = _projectID || 0;
 
-  let cardData = [
-    projects[1],
-    'Everyday Chores changes',
-    'Making your bed.',
-    '2020-04-07',
-    'High',
-    activities.length
-  ];
+  // console.log(_projectID);
+  // let localStorageProjects = JSON.parse(localStorage.getItem("projects") || "[]");
+  projects = JSON.parse(localStorage.getItem("projects") || "[]");
+  
+  $('#choose-project-list').append(`<option disabled selected value> -- select an option -- </option>`);
+  for(let i = 0; i < projects.length; i += 1) {
+    $('#choose-project-list').append(`<option value="${projects[i].index}">${projects[i].name}</option>`);
+  }
+  $('#choose-project-list').append(`<option value="${-1}">All the projects</option>`);
+
+  $('#choose-project-list').change(() => {
+    displayCards($('#choose-project-list').val());
+  });
 
   let localStorageCards = JSON.parse(localStorage.getItem("activities") || "[]");
+
   localStorageCards = localStorageCards.map((card) => {
-    if(card){
+    if (card) {
       let _todoCard = todoCard();
       _todoCard.setCardByLiteral(card);
-      return card = _todoCard;
+      return _todoCard;
     }
+
     return null;
-  })
+  });
+
+  console.log(projects);
+
+  for (let i = 0; i < localStorageCards.length; i++) {
+    if (localStorageCards[i]) {
+      console.log(localStorageCards[i].getProject().index);
+    }
+  }
+
   activities = localStorageCards;
-  //todo.setCard(cardData);
-  //activities.push(todo);
-  //console.log(activities);
-  displayCards();
-  // $('.todo-table').append(card.getHTML());
+  
+  displayCards(-1);
 });
 
